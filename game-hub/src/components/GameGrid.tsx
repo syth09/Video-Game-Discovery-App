293dc93b37563e2b509bcf32e11e05e279814c8f
syth09 <tatuanphong616@gmail.com>
2024-09-25 23:40:19 +0700

    Installing the react icons library for implementing the platform icons

--- game-hub/src/components/GameGrid.tsx
+++ game-hub/src/components/GameGrid.tsx
@@ -9,7 +9,7 @@ const GameGrid = () => {
     <>
       {error && <Text>{error}</Text>}
       <SimpleGrid
-        columns={{ sm: 1, md: 2, lg: 3, xl: 5 }}
+        columns={{ sm: 1, md: 2, lg: 3, "2xl": 5 }}
         padding="10px"
         spacing={10}
       >
