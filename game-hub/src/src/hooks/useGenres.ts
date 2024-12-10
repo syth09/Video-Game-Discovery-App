3366c651136a4fa5bcb15013b8def653c728ed20
syth09 <tatuanphong616@gmail.com>
2024-12-10 22:58:26 +0700

    Fetching live platform data and fixed up the 'any' type on genreList

--- game-hub/src/src/hooks/useGenres.ts
+++ game-hub/src/src/hooks/useGenres.ts
@@ -1,6 +1,7 @@
 import { useQuery } from "@tanstack/react-query";
 import apiClient from "../services/api-client";
 import genres from "../data/genres";
+import { FetchResponse } from "./useData";
 
 export interface Genre {
   id: number;
@@ -11,7 +12,7 @@ export interface Genre {
 const useGenres = () =>
   useQuery({
     queryKey: ["genres"],
-    queryFn: () => apiClient.get("/genres").then((res) => res.data),
+    queryFn: () => apiClient.get<FetchResponse<Genre>>("/genres").then((res) => res.data),
     staleTime: 24 * 60 * 60 * 1000, // 24h
     initialData: { count: genres.length, results: genres}
   });
