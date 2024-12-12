902ba93a2d82ab8ea4aa1117cb41617913f2bfdf
syth09 <tatuanphong616@gmail.com>
2024-12-12 11:10:19 +0700

    Little change on the display columns

--- game-hub/src/components/GameGrid.tsx
+++ game-hub/src/components/GameGrid.tsx
@@ -17,7 +17,7 @@ const GameGrid = ({ gameQuery }: Props) => {
 
   return (
     <SimpleGrid
-      columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
+      columns={{ sm: 1, md: 2, lg: 3, '2xl': 4 }}
       padding="10px"
       spacing={6}
     >
